87c4168d0b68a491c556b515a3d57bbbd842b517
kornienkoanton <anton.kornienko@nlplogix.com>
2017-10-09 10:41:09 -0400

    decorator

--- week2/decorator_to_json.py
+++ week2/decorator_to_json.py
@@ -1,7 +1,9 @@
-@to_json
-def get_data():
-  return {
-    'data': 42
-  }
-  
-get_data()  # вернёт '{"data": 42}'
+import json, functools
+
+def to_json (func):
+	@functools.wraps(func)
+	def wrapped (*args,**kwargs):
+		result = func(*args,**kwargs)
+		return json.dumps(result)
+	 
+	return wrapped
