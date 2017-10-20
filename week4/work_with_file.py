967ac3355631a7c17bb73b6222fb5083592321a7
kornienkoanton <anton.kornienko@nlplogix.com>
2017-10-20 10:44:31 -0400

    complited first task

--- week4/work_with_file.py
+++ week4/work_with_file.py
@@ -1,38 +1,36 @@
 import os, tempfile
 
 class File:
-   """File operator"""
+	"""File operator"""
 
-   def __init__(self, file_path):
-      self.file_path = file_path
+	def __init__(self, file_path):
+		self.file_path = file_path
 
-   def _read(self):
-      with open(self.file_path, "r+") as f:
-         return f.read()
-   
-   def __str__(self):
-      return self.file_path
+	def __str__(self):
+		return self.file_path
 
-   def write(self, obj):
-      with open (self.file_path, "w") as f:
-         f.write(obj+"\n")
-   
-   def __add__(self, obj):
-      with open(os.path.join(tempfile.gettempdir, "temp.txt"), "w+") as f:
-         text = self._read()
-         f.write(text)
-      return text
+	def _read(self):
+		with open(self.file_path, "r") as f:
+			return f.read()
 
-   def __getitem__(self, index):
-      return self._read()[index]
+	def write(self, obj):
+		with open (self.file_path, "w") as f:
+			return f.write(obj)
+
+	def __add__(self, obj):
+		new_file = File(os.path.join(tempfile.gettempdir(), "temp.txt"))
+		new_file.write(self._read() + "\n" + obj._read())
+		return new_file
+
+	def __getitem__(self, index):
+		return self._read().split()[index]
 
 if __name__ == "__main__":
-   a = File("/home/anton/Documents/python notebook/training/text1.txt")
-   b = File("/home/anton/Documents/python notebook/training/text2.txt")
-   a.write("asd")
-   b.write("zxc")
-   print (a)
-   print(a + b)
-
-
-
+	a = File("/home/anton/Documents/python notebook/training/text1.txt")
+	b = File("/home/anton/Documents/python notebook/training/text2.txt")
+	a.write("asd")
+	b.write("zxc")
+	c = a + b
+	print(a,b,c)
+	for row in c:
+		print(row)
